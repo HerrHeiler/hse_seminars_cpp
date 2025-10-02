013994fbe30490c9d9cf878c1c9cc868dda43231
Miroslava Melnichuk <miroslavamel@Miroslavas-MacBook-Pro.local>
2025-10-02 19:46:55 +0300

    temporary

--- contest4/taskH.cpp
+++ contest4/taskH.cpp
@@ -42,7 +42,21 @@ int main() {
                 std::cout << '\n';
             }
         }
+        //std::cout << '\n';
     }
+    // }
+    // else {
+    //     for (int j = 0; j < n; ++j){
+    //     for (int i = 0; i < m; ++i){
+
+    //         std::cout << s[i][j];
+    //         if (j != m-1) {
+    //             std::cout << ' ';
+    //         }
+    //     }
+    //     std::cout << '\n';
+    // }
+    // }
 
     for (int i = 0; i < n; ++i){
         delete[] s[i];
