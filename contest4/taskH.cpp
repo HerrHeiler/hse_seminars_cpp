d04135ad7f2696537bb6e7108eb94993a1a17a0d
Miroslava Melnichuk <miroslavamel@Miroslavas-MacBook-Pro.local>
2025-10-04 14:40:24 +0300

    cotest 4 successfully solved

--- contest4/taskH.cpp
+++ contest4/taskH.cpp
@@ -3,62 +3,92 @@
 int main() {
     int n;
     int m;
+
     std::cin >> n >> m;
-    int** s= new int* [n];
 
-    for (int i = 0; i < n; ++i) s[i] = new int[m]();
-
+    int **s= new int*[std::max(n, m)];
+    for (int i=0; i<std::max(n, m); ++i){
+        s[i] = new int[std::min(n, m)]();
+    }
+    if (n >= m){
     int k = 0;
     int kr = 1;
-    for (int i = 0; i < m; ++i){
-        for (int j = kr-1; j >= 0; --j){
+    for (int i = 0; i < std::min(n, m); ++i){
+        for (int j = kr-1; j >= 0; j--){
             s[i-j][j] = k;
             ++k;
         }
         ++kr;
     }
     --kr;
-    for (int i = m; i < n; ++i){
-        for (int j = kr-1; j >= 0; --j){
+    for (int i = std::min(n, m); i < std::max(n, m); ++i){
+        for (int j = kr-1; j >= 0; j--){
             s[i-j][j] = k;
             ++k;
         }
     }
     --kr;
-    for (int i = 1; i < m; ++i){
-        for (int j = kr-1; j >= 0; --j){
-            s[n-j-1][i+j] = k;
+    for (int i = 1; i < std::min(n, m); ++i){
+        for (int j = kr-1; j >= 0; j--){
+            s[std::max(n, m)-j-1][i+j] = k;
+            // std::cout << n << ' ' << i << ' ' << j << ' ' << k << ' ' << n-i+j << '\n';
             ++k;
         }
         --kr;
     }
+} else {
+    int k = 0;
+    int kr = 1;
+    for (int i = 0; i < std::min(n, m); ++i){
+        for (int j = 0; j < kr; ++j){
+            s[i-j][j] = k;
+            ++k;
+        }
+        ++kr;
+    }
+    --kr;
+    for (int i = std::min(n, m); i < std::max(n, m); ++i){
+        for (int j = 0; j < kr; ++j){
+            s[i-j][j] = k;
+            ++k;
+        }
+    }
+    --kr;
+    for (int i = 1; i < std::min(n, m); ++i){
+        for (int j = 0; j < kr; ++j){
+            s[std::max(n, m)-j-1][i+j] = k;
+            // std::cout << n << ' ' << i << ' ' << j << ' ' << k << ' ' << n-i+j << '\n';
+            ++k;
+        }
+        --kr;
+    }
+}
 
-    for (int i = 0; i < n; ++i){
-        for (int j = 0; j < m; ++j){
+    if (std::max(n, m) == n){
+    for (int i=0; i<n; ++i){
+        for (int j=0; j<m; ++j){
             std::cout << s[i][j];
             if (j != m-1) {
                 std::cout << ' ';
-            } else {
-                std::cout << '\n';
             }
         }
-        //std::cout << '\n';
+        std::cout << '\n';
     }
-    // }
-    // else {
-    //     for (int j = 0; j < n; ++j){
-    //     for (int i = 0; i < m; ++i){
+    }
+    else {
+        for (int j=0; j<n; ++j){
+        for (int i=0; i<m; ++i){
 
-    //         std::cout << s[i][j];
-    //         if (j != m-1) {
-    //             std::cout << ' ';
-    //         }
-    //     }
-    //     std::cout << '\n';
-    // }
-    // }
+            std::cout << s[i][j];
+            if (j != m-1) {
+                std::cout << ' ';
+            }
+        }
+        std::cout << '\n';
+    }
+    }
 
-    for (int i = 0; i < n; ++i){
+    for (int i=0; i<n; ++i){
         delete[] s[i];
     }
     return 0;
--- contest4/taskJ.cpp
+++ contest4/taskJ.cpp
@@ -2,15 +2,33 @@
 
 int main() {
     int N, M, W;
-    int mine [N][M];
+    const int size = 32;
+    std::cin >> N >> M >> W;
+    int mine[size][size] = {0};
+
     for (int l = 0; l < W; ++l) {
         int t1, t2;
         std::cin >> t1 >> t2;
         mine[t1 - 1][t2 - 1] = 1;
     }
-    for (int i = 0; i < M; ++i) {
-        for (int j = 0; j < N; ++j) {
-            //if ()
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            if (mine[i][j] == 1) {
+                std::cout << '*';
+            } else {
+                int t = 0;
+                if (i != 0) t += mine[i - 1][j];
+                if (j != 0) t += mine[i][j - 1];
+                if (i != 0 && j != 0) t += mine[i - 1][j - 1];
+                if (i < N - 1) t += mine[i + 1][j];
+                if (j < M - 1) t += mine[i][j + 1];
+                if (i < N - 1 && j < M - 1) t += mine[i + 1][j + 1];
+                if (i != 0 && j < M - 1) t += mine[i - 1][j + 1];
+                if (i < N - 1 && j != 0) t += mine[i + 1][j - 1];
+                std::cout << t;
+            }
+            if (j != M - 1) std::cout << ' ';
+            else std::cout << ' ' << '\n';
         }
     }
     return 0;
