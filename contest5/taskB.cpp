b706b8105d49c5abf5d980abfc8371e98c5103a3
Miroslava Melnichuk <miroslavamel@Miroslavas-MacBook-Pro.local>
2025-10-06 19:48:26 +0300

    contest 5 is created

---
+++ contest5/taskB.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+bool is_palindrome(std::string _stroka) {
+    
+    return false;
+}
+
+int main() {
+    std::string given;
+    std::cin >> given;
+    std::cout << is_palindrome(given);
+    return 0;
+}
