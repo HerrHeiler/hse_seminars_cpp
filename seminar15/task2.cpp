7beeed7c5be727020948541b8d6d6c391fcbec3e
Miroslava Melnichuk <miroslavamel@Mac.Dlink>
2025-11-30 16:01:06 +0300

    sem 15 is added

---
+++ seminar15/task2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+class ArithmeticProgression {
+    double start;
+    double step;
+    size_t size;
+    std::vector<double> sequence;
+
+public:
+    ArithmeticProgression() : start(0), step(1), size(0) {}
+    
+    ArithmeticProgression(double s, double st, size_t sz) 
+        : start(s), step(st), size(sz) {
+        sequence.resize(sz);
+        for (size_t i = 0; i < sz; ++i) {sequence[i] = start + i * step;}
+    }
+    
+    size_t getSize() const {return size;}
+    
+    double operator[](int index) const {
+        if (index >= 0 && static_cast<size_t>(index) < size) {return sequence[index];}
+        return 0.0;
+    }
+};
+
+int main() {
+    ArithmeticProgression test1(3, 3.5, 45);
+    std::cout << "Size: " << test1.getSize() << std::endl;
+    for (int i = 0; i < 7; ++i) {std::cout << test1[i] << std::endl;}
+    return 0;
+}
